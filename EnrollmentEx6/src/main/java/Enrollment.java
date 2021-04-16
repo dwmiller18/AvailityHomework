286253628742a9de27ed925ce5d0d52ca0ea58e4
Dustin Miller <dwmiller@anderson.edu>
2021-04-16 07:03:31 -0700

    final changes for enrollment exercise

--- EnrollmentEx6/src/main/java/Enrollment.java
+++ EnrollmentEx6/src/main/java/Enrollment.java
@@ -11,7 +11,7 @@ public class Enrollment {
     private final static int idPos = 0;
     private final static int fnPos = 1;
     private final static int lnPos = 2;
-    private final static int vPos  = 3;
+    private final static int vPos = 3;
     private final static int icPos = 4;
     private static List<String> insCos = new ArrayList<String>();
 
@@ -19,11 +19,12 @@ public class Enrollment {
         /* Get the filename to parse */
         Scanner input = new Scanner(System.in);
         System.out.println("What is the file path to your csv file? " +
-                "Ex: /Users/Bob/Desktop/text.txt");
+                "Ex: /Users/Bob/Desktop/test.csv");
         String fileName = input.nextLine();
 
         /* Get the location to store new files */
-        System.out.println("Enter the file path along with folder name where you want new files stored?");
+        System.out.println("Enter the file path along with folder name where you want new files stored?" +
+                " WARNING: IF THESE FILES AND FOLDERS EXIST, NEW FILES WILL NOT BE CREATED");
         String path = input.nextLine();
         File dirLocation = new File(path);
 
@@ -31,35 +32,38 @@ public class Enrollment {
         List<String[]> records = readCSVFile(fileName);
 
         /* Get list of insurance companies */
-        for(int i = 0; i < records.size(); i++) {
-            if(!insCos.contains(records.get(i)[icPos])) {
+        for (int i = 0; i < records.size(); i++) {
+            if (!insCos.contains(records.get(i)[icPos])) {
                 insCos.add(records.get(i)[icPos]);
             }
         }
 
         /* Separate enrollees by insurance company into it's own file */
         /* Create Directory */
-        if(!dirLocation.exists()) {
+        if (!dirLocation.exists()) {
             dirLocation.mkdirs();
         }
 
-        /* Sort contents of each file by last and first name (ascending) and remove duplicates with same ID and Ins Co */
+        /* Sort contents of each file by last and first name (ascending)
+            and remove duplicates with same ID and Ins Co */
         List<String[]> sortedResults = sortArray(records);
 
         /* Create files */
-        for(String ins : insCos) {
+        for (String ins : insCos) {
             String temp = dirLocation + "/" + ins + ".txt";
             File f = new File(temp);
             try {
-                if(!f.exists()) {
-                    f.createNewFile();
-                    PrintWriter pw = new PrintWriter(f, "UTF-8");
-                    for(int i = 0; i < sortedResults.size(); i++) {
-                        if(sortedResults.get(i)[icPos].equals(ins)) {
-                            pw.println(Arrays.toString(sortedResults.get(i)));
+                if (!f.exists()) {
+                    boolean created = f.createNewFile();
+                    if (created) {
+                        PrintWriter pw = new PrintWriter(f, "UTF-8");
+                        for (int i = 0; i < sortedResults.size(); i++) {
+                            if (sortedResults.get(i)[icPos].equals(ins)) {
+                                pw.println(Arrays.toString(sortedResults.get(i)));
+                            }
                         }
+                        pw.close();
                     }
-                    pw.close();
                 }
             } catch (Exception e) {
                 e.printStackTrace();
@@ -68,14 +72,14 @@ public class Enrollment {
     }
 
     /**
-     *
-     * @param file
-     * @return
+     * readCSVFile(String) - Reads in the CSV file.
+     * @param file - The file to be read.
+     * @return - A list of string arrays from the CSV file.
      */
-    public static List<String[]> readCSVFile(String file) {
+    private static List<String[]> readCSVFile(String file) {
         List<String[]> records = new ArrayList<String[]>();
 
-        try(BufferedReader br = new BufferedReader(new FileReader(file))) {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             String line = "";
             while ((line = br.readLine()) != null) {
                 records.add(line.split(","));
@@ -88,50 +92,58 @@ public class Enrollment {
     }
 
     /**
-     *
-     * @param list
-     * @return
+     * sortArray(List<String[]>) - Takes a list of string arrays and sorts them
+     * based on last name and first name.  Also, if the ID number and insurance
+     * company are the same, it will remove the array with the lower version number.
+     * @param list - The list to be sorted.
+     * @return result - The newly sorted list.
      */
-    public static List<String[]> sortArray(List<String[]> list) {
-        List<String[]> result = new ArrayList<>();
+    private static List<String[]> sortArray(List<String[]> list) {
+        boolean sorted = true;
 
         /* Sort by last name then first name */
-        for(int i = 0; i < list.size(); i++) {
-            for(int j = i + 1; j < list.size(); j++) {
-                if(list.get(i)[lnPos].compareTo(list.get(j)[lnPos]) > 0) {
-                    String[] temp = list.get(i);
-                    list.set(i, list.get(j));
-                    list.set(j, temp);
-                } else if (list.get(i)[lnPos].equals(list.get(j)[lnPos])) {
-                    if(list.get(i)[fnPos].compareTo(list.get(j)[fnPos]) > 0) {
+        while (sorted) {
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = i + 1; j < list.size(); j++) {
+                    if (list.get(i)[lnPos].toUpperCase().compareTo(list.get(j)[lnPos].toUpperCase()) > 0) {
                         String[] temp = list.get(i);
                         list.set(i, list.get(j));
                         list.set(j, temp);
+                        sorted = false;
+                    } else if (list.get(i)[lnPos].toUpperCase().equals(list.get(j)[lnPos].toUpperCase())) {
+                        if (list.get(i)[fnPos].compareTo(list.get(j)[fnPos]) > 0 &&
+                                !list.get(i)[fnPos].equals(list.get(j)[fnPos])) {
+                            String[] temp = list.get(i);
+                            list.set(i, list.get(j));
+                            list.set(j, temp);
+                            sorted = false;
+                        }
                     }
-                } else {
-                    //do nothing
                 }
             }
         }
+
+        sorted = true;
 
         /* If id and ins co is the same, remove the one with the lower version number */
-        for(int i = 0; i < list.size(); i++) {
-            for(int j = i + 1; j < list.size(); j++) {
-                if (list.get(i)[idPos] == list.get(j)[idPos] &&
-                        list.get(i)[icPos].equals(list.get(j)[icPos])) {
-                    int temp1 = Integer.parseInt(list.get(i)[vPos]);
-                    int temp2 = Integer.parseInt(list.get(j)[vPos]);
-                    if(temp1 < temp2) {
-                        list.remove(i);
-                    } else {
-                        list.remove(j);
+        while(sorted) {
+            for (int i = 0; i < list.size(); i++) {
+                for (int j = i + 1; j < list.size(); j++) {
+                    if (list.get(i)[idPos].equals(list.get(j)[idPos]) &&
+                            list.get(i)[icPos].toUpperCase().equals(list.get(j)[icPos].toUpperCase())) {
+                        int temp1 = Integer.parseInt(list.get(i)[vPos]);
+                        int temp2 = Integer.parseInt(list.get(j)[vPos]);
+                        if (temp1 < temp2) {
+                            list.remove(i);
+                            sorted = false;
+                        } else {
+                            list.remove(j);
+                            sorted = false;
+                        }
                     }
                 }
             }
         }
-
-        result = list;
-
-        return result;
+        return list;
     }
 }
